61dd9d5a8c88883d1ec478b8c31c801dbde3f212
Rupert <47789156+dublowduck@users.noreply.github.com>
2020-05-04 15:01:58 +0100

    Added operator detection
    Added operator detection so you can add, subtract, times and divide. The division, however, still doesn`t work.

--- calculator.py
+++ calculator.py
@@ -1,6 +1,22 @@
-from math import *
+#import crap that may be needed
+#from math import *
+#ask the user what they are trying to do
 num1 = input("Enter a number: ")
 num2 = input("Enter another number: ")
-result = float(num1) + float(num2)
+operator = input("Enter an operator: ")
+#calculate the crap that the user entered
 
-print(result)
+#if times, then times
+if operator == "*":
+    result = float(num1) * float(num2)
+#if division, then divide
+if operator == "/":
+    result = int(num1) / int(num2)
+#if addition, that add it together y` ejit
+if operator == "+":
+    result = float(num1) + float(num2)
+# if subtraction, then take the numbers away from each other
+if operator == "-":
+    result = float(num1) - float(num2)
+# show them all the hardwork, they could`nt be bothered to do
+print(float(result))
