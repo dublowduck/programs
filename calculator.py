7c3232d64605c14c8af6bb4d1b7dcb794313c887
Rupert <47789156+dublowduck@users.noreply.github.com>
2020-05-06 20:13:28 +0100

    Division now uses floating-point numbers
    Division now uses floating-point numbers instead of integers.

--- calculator.py
+++ calculator.py
@@ -12,7 +12,7 @@ if operator == "*":
     result = float(num1) * float(num2)
 #if division, then divide
 if operator == "/":
-    result = int(num1) / int(num2)
+    result = float(num1) / float(num2)
 #if addition, that add it together
 if operator == "+":
     result = float(num1) + float(num2)
